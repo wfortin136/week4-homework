f7927b11f21437e3e05fcef589e3063fccb36d62
B410 <wjf136@gmail.com>
2015-07-18 16:27:25 -0500

    Completed HW4

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,15 @@ Rails.application.routes.draw do
   root 'places#index'
 
   get '/places' => 'places#index'
+  
   get '/places/new' => 'places#new'
-  get '/places/submit_new' => 'places#submitted'
+  post '/places' => 'places#create'
   get '/places/:id' => 'places#show'
-  get '/places/:id/delete' => 'places#delete'
+  
   get '/places/:id/edit' => 'places#edit'
-  get '/places/:id/submit_edit' => 'places#update'
+  patch '/places/:id' => 'places#update'
+  delete '/places/:id' => 'places#destroy'
 
-  get '/reviews/:place_id/submit_new'  => 'reviews#create'
+  post '/reviews/:place_id'  => 'reviews#create'
 
 end
